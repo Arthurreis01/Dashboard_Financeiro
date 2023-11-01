9ef7d8885590be1bbe15beaa1f82e748751873ce
Arthurreis01 <147757007+Arthurreis01@users.noreply.github.com>
2023-11-01 11:05:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,9 @@ import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-import openai
-import time
 
 
 st.title("`Painel de controle financeiro da Empresa X`")
-
-# Adicionando conteúdo ao sidebar
-
 
 #coletar a base de dados
 df = pd.read_csv("C:\\Users\\arthu\\Downloads\\relatorio (4).csv", sep=";")
@@ -29,8 +24,6 @@ colunas_para_excluir = ['Código','N. Fiscal', 'Dispositivo']
 df = df.drop(colunas_para_excluir, axis=1)
 df= df.sort_values('Data')
 
-#excluir linhas com cancelada
-
 #colocar datas em formato de periodo mensal
 df["Dia"] = df["Data"].apply(lambda x: str(x.month) + "-" + str(x.day))
 df= df.sort_values('Dia')
@@ -44,10 +37,6 @@ st.sidebar.subheader('Escolha o período')
 Periodo = st.sidebar.select_slider("Mês", df["Data"].unique())
 df_filtered = df[df["Data"] == Periodo]
 
-
-
-
-
 #colocar em modo visual (vendas, ranking de categorias (despesas e receitas), indicadores, orçamento x realizado)
 #vendas por mês
 st.markdown('### INDICADORES DA EMPRESA')
@@ -55,7 +44,6 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Valor em caixa","RS 5.000,00", "R$ 5.600,00")
 col2.metric("ROI", "5 ANOS", "2 ANOS")
 col3.metric("PONTO DE EQULIBRIO", "R$ 1.950,00", "R$ 2.600,00")
-
 
 
 col3, col4 = st.columns(2)
@@ -71,7 +59,6 @@ fig.add_trace(go.Bar(x=df["Data"], y=df["Valor"], name="Vendas (Barras)"), row=1
 
 # Adicionar o gráfico de linhas na segunda linha
 fig.add_trace(go.Scatter(x=df["Data"], y=df["Valor"], mode="lines", name="Vendas (Linhas)"), row=2, col=1)
-
 
 
 fig_vendas = px.bar(df_filtered, x="Dia", y="Valor", title= "Vendas por dia em cada mês")
