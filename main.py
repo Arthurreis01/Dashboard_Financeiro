cc6a7a78e69b8a86ef3c27ca5fd5783be95ef6eb
Arthurreis01 <147757007+Arthurreis01@users.noreply.github.com>
2023-11-02 01:28:13 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-st.set_page_config(layout="wide")
 st.title("`Painel de controle financeiro da Empresa X`")
 
+# Adicionando conteúdo ao sidebar
+
 #coletar a base de dados
-df = pd.read_csv("relatorio (4).csv", sep=";")
+df = pd.read_csv("C:\\Users\\arthu\\Downloads\\relatorio (4).csv", sep=";")
 #separar a data da hora criando uma nova coluna #criar uma coluna só com os dias
 df['Data']=pd.to_datetime(df['Data'])
 #nova coluna
@@ -24,6 +25,8 @@ colunas_para_excluir = ['Código','N. Fiscal', 'Dispositivo']
 df = df.drop(colunas_para_excluir, axis=1)
 df= df.sort_values('Data')
 
+#excluir linhas com cancelada
+
 #colocar datas em formato de periodo mensal
 df["Dia"] = df["Data"].apply(lambda x: str(x.month) + "-" + str(x.day))
 df= df.sort_values('Dia')
@@ -37,6 +40,10 @@ st.sidebar.subheader('Escolha o período')
 Periodo = st.sidebar.select_slider("Mês", df["Data"].unique())
 df_filtered = df[df["Data"] == Periodo]
 
+st.sidebar.subheader('Veja se sua ideia vale um bom negócio com a calculadora automática !')
+st.sidebar.markdown("[**Calculadora de bons negócios**](http://192.168.0.168:8501)")
+
+
 #colocar em modo visual (vendas, ranking de categorias (despesas e receitas), indicadores, orçamento x realizado)
 #vendas por mês
 st.markdown('### INDICADORES DA EMPRESA')
@@ -44,6 +51,7 @@ col1, col2, col3 = st.columns(3)
 col1.metric("Valor em caixa","RS 5.000,00", "R$ 5.600,00")
 col2.metric("ROI", "5 ANOS", "2 ANOS")
 col3.metric("PONTO DE EQULIBRIO", "R$ 1.950,00", "R$ 2.600,00")
+
 
 
 col3, col4 = st.columns(2)
@@ -61,13 +69,9 @@ fig.add_trace(go.Bar(x=df["Data"], y=df["Valor"], name="Vendas (Barras)"), row=1
 fig.add_trace(go.Scatter(x=df["Data"], y=df["Valor"], mode="lines", name="Vendas (Linhas)"), row=2, col=1)
 
 
+
 fig_vendas = px.bar(df_filtered, x="Dia", y="Valor", title= "Vendas por dia em cada mês")
 col3.plotly_chart(fig_vendas,use_container_width=True)
 
 fig_receitas = px.bar(df_filtered,x="Valor",y="Vendedor",orientation="h", title="Vendas por categoria", color="Vendedor")
 col4.plotly_chart(fig_receitas, use_container_width=True)
-
-
-
-
-
